956633ae5d3a86916bc50ed6891e070f5bb8c465
keijiro <kuroiwa.keijiro@gmail.com>
2014-03-18 03:36:54 +0900

    検査例外をキャッチせずに、スローするように変更。

--- src/main/java/net/kuronicle/iremocon4j/IRemoconClient.java
+++ src/main/java/net/kuronicle/iremocon4j/IRemoconClient.java
@@ -11,42 +11,30 @@ import org.apache.commons.net.telnet.TelnetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 
+ * @author KUROIWA, Keijiro
+ * 
+ */
 public class IRemoconClient {
 
-    /**
-     * iRemoconのデフォルトポート番号。
-     */
     private static final int DEFAULT_PORT = 51013;
 
-    /**
-     * iRemoconコマンド：接続確認用。
-     */
     public static final String COMMAND_CHECK_CONNECTION = "au";
-    
-    /**
-     * iRemoeconコマンド：赤外線発信。
-     */
-    public static final String COMMAND_SEND_IR = "is";
-    
-    /**
-     * ロガー。
-     */
-    private static Logger logger = LoggerFactory.getLogger(IRemoconClient.class);
 
-    /**
-     * iRemoconのホスト名。
-     */
+    public static final String COMMAND_SEND_IR = "is";
+
+    private static Logger logger = LoggerFactory
+            .getLogger(IRemoconClient.class);
+
     private String hostName;
 
-    /**
-     * iRemoconのポート番号。
-     */
     private int port;
 
-    /**
-     * Tenlneクライアント。
-     */
     private TelnetClient telnetClient;
+
+    private String telnetSendCommandString = System
+            .getProperty("line.separator");
 
     public IRemoconClient(String hostName) {
         this(hostName, DEFAULT_PORT);
@@ -58,41 +46,39 @@ public class IRemoconClient {
         this.telnetClient = new TelnetClient();
     }
 
-    public String sendCommand(String commandName, String... parameter) {
+    /**
+     * iRemoconにコマンドを送信する。事前に{@link #connect()}でiRemoconと接続しておくこと。
+     * 
+     * @param commandName
+     * @param parameter
+     * @return
+     * @throws IOException
+     */
+    public String sendCommand(String commandName, String... parameter)
+            throws IOException {
         String iRemoconCommand = buildIRemoconCommand(commandName, parameter);
         String telnetResponse = sendTelnetCommand(iRemoconCommand);
         return telnetResponse;
     }
 
-    private String sendTelnetCommand(String iRemoconCommand) {
-        Writer requestWriter = new OutputStreamWriter(telnetClient.getOutputStream());
-        BufferedReader responseReader = new BufferedReader(new InputStreamReader(telnetClient.getInputStream()));
-        String telnetResponse;
+    private String sendTelnetCommand(String iRemoconCommand) throws IOException {
+        Writer requestWriter = new OutputStreamWriter(
+                telnetClient.getOutputStream());
+        BufferedReader responseReader = new BufferedReader(
+                new InputStreamReader(telnetClient.getInputStream()));
+
         try {
             logger.debug("send command. command=" + iRemoconCommand);
             requestWriter.write(iRemoconCommand);
             requestWriter.flush();
-            Thread.sleep(1000);
             
-            telnetResponse = responseReader.readLine();
+            String telnetResponse = responseReader.readLine();
             logger.debug("receive response. response=" + telnetResponse);
             return telnetResponse;
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } finally {
-            try {
-                requestWriter.close();
-                responseReader.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            requestWriter.close();
+            responseReader.close();
         }
-        return null;
     }
 
     private String buildIRemoconCommand(String commandName, String[] parameter) {
@@ -102,32 +88,31 @@ public class IRemoconClient {
         for (int i = 0; i < parameter.length; i++) {
             commandBuilder.append(";" + parameter[i]);
         }
-        commandBuilder.append(System.getProperty("line.separator"));
+        commandBuilder.append(telnetSendCommandString);
         return commandBuilder.toString();
     }
 
-    public void connect() {
-        try {
-            logger.debug(String.format("connect iRemocon. hostName=%s, port=%d", hostName, port));
-            telnetClient.connect(hostName, port);
-        } catch (SocketException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    /**
+     * iRemoconとTelnet接続する。
+     * 
+     * @throws SocketException
+     * @throws IOException
+     */
+    public void connect() throws SocketException, IOException {
+        logger.debug(String.format("connect iRemocon. hostName=%s, port=%d",
+                hostName, port));
+        telnetClient.connect(hostName, port);
     }
 
-    public void disconnect() {
-        if (telnetClient != null) {
-            try {
-                logger.debug("disconnect iRemoecon.");
-                telnetClient.disconnect();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+    /**
+     * iRemoconと切断する。iRemoconの同時接続数は1であるため、接続後は必ず切断すること。
+     * 
+     * @throws IOException
+     */
+    public void disconnect() throws IOException {
+        if (telnetClient != null && telnetClient.isConnected()) {
+            logger.debug("disconnect iRemoecon.");
+            telnetClient.disconnect();
         }
     }
 
--- src/test/java/it/IRemoteClientIntegrationTest.java
+++ src/test/java/it/IRemoteClientIntegrationTest.java
@@ -1,18 +1,22 @@
 package it;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertThat;
 import net.kuronicle.iremocon4j.IRemoconClient;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author KUROIWA, Keijiro
+ *
+ */
 public class IRemoteClientIntegrationTest {
 
     private static final String IREMOCON_HOST_NAME = "192.168.11.50";
 
     @Test
-    public void testCheckConnection() {
+    public void testCheckConnection() throws Exception {
         String commandName = IRemoconClient.COMMAND_CHECK_CONNECTION;
         String expected = "ok";
 
@@ -29,10 +33,10 @@ public class IRemoteClientIntegrationTest {
     }
 
     @Test
-    public void testSendIr() {
+    public void testSendIr() throws Exception {
         String commandName = IRemoconClient.COMMAND_SEND_IR;
         String parameter = "1";
-        String expected = "ok";
+        String expected = "is;ok";
 
         IRemoconClient iRemoconClient = new IRemoconClient(IREMOCON_HOST_NAME);
         String actual;
